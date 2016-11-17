07575a40dd7ddbde1c85a0765a94b4e2b2f2289d
frankurt <frank7cevallos@hotmail.com>
2016-11-17 14:26:40 -0500

    Creacion de proyecto
    17/11/16

---
+++ html/src/com/palagames/tuserietv/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.palagames.tuserietv.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.palagames.tuserietv.game;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new game();
+        }
+}
