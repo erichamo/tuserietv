301abd596b1e6d175f2daec0295e6e30b491e7f8
frankurt <frank7cevallos@hotmail.com>
2016-11-18 01:51:50 -0500

    Generacion Lista de series
    se autogeneran la listas de series, y se añaden automaticamente sus
    iconos.

---
+++ Assets/Scrips/Serie.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Serie : MonoBehaviour {
+
+	public Text nombre;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scrips/SerieController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SerieController : MonoBehaviour {
+
+	public Serie Serie_Base;
+	public TextAsset lista_series;
+
+	private int num_Series;
+
+	void Start () {
+		num_Series = 0;
+		crear_ListaSeries();
+	}
+
+	private void crear_ListaSeries(){
+		string content = lista_series.text;
+		string valor = "";
+
+		while(valor!=null){
+			GameObject serieTemp = (GameObject)(Instantiate(Serie_Base.gameObject,transform));
+			serieTemp.name = (content.Split("\n"[0])[num_Series].ToString());
+			serieTemp.GetComponent<Serie>().nombre.text = serieTemp.name;
+			serieTemp.transform.localPosition = new Vector3(Serie_Base.gameObject.transform.localPosition.x,4.5f-1.5f*num_Series,Serie_Base.gameObject.transform.localPosition.z);
+
+			serieTemp.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Series_Images/"+serieTemp.name.Remove(serieTemp.name.Length-1,1));
+
+			num_Series++;
+			if(content.Split("\n"[0])[num_Series] == "*") break;
+		}
+		Destroy(Serie_Base.gameObject);
+	}
+
+}
